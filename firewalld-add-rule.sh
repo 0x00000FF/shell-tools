a820c5ca3f6788b2cbe9dfab2f76ef53ea7f7f63
Kangjun Heo <knowledge@o.cnu.ac.kr>
2023-03-20 09:07:51 +0900

    Create firewalld-add-rule.sh

---
+++ firewalld-add-rule.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# firewalld rich rule assisting script
+# 
+# 1st arg  : zone (default 'public')
+# 2nd arg  : ip address
+# 3rd arg  : port
+# 4th args : tcp/udp
+# 5th args : accept/reject
+
+zone=${1:-'public'}
+
+if [[ -z $2 || -z $3 || -z $4 || -z $5 ]]; then
+    echo "Querying Firewall List..."
+else
+    firewall-cmd --permanent --zone=$zone --add-rich-rule="rule family=\"ipv4\" source address=\"$2\" port port=\"$3\" protocol=\"$4\" $5"
+    firewall-cmd --reload
+fi
+
+firewall-cmd --list-all
