ccf4126a3572b84c3a6e076cbe12d821b6014a6f
Kangjun Heo <kheo@sidelcorp.com>
2024-02-27 23:18:47 +0900

    brief comment

--- certbot-porkbun-cleanup-hook.sh
+++ certbot-porkbun-cleanup-hook.sh
@@ -5,6 +5,7 @@ APISEC=${PORKBUN_APISEC}
 
 DOMAIN=${DOMAIN_NAME}
 
+# keep in mind that auth-hook.sh exports RECORD_ID 
 curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
      -d "{ \"secretapikey\": \"${APISEC}\", \"apikey\": \"${APIKEY}\"" \
      "https://porkbun.com/api/json/v3/dns/delete/${DOMAIN}/${RECORD_ID}"
