0fd55b4beee2e3c01ae20ab5c1702d5548f33aa0
Kangjun Heo <kheo@sidelcorp.com>
2024-02-27 22:09:52 +0900

    certbot renew

---
+++ certbot-wildcard-renew.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -z "${DOMAIN_NAME}" ]; then
+    echo "domain name is not specified"
+    exit
+fi
+
+if [ -z "${MAIL_ADDRESS}" ]; then
+    echo "mail address is not specified"
+    exit
+fi
+
+# prepare certbot-auth-hook.sh and certbot-cleanup-hook.sh by yourselves
+certbot certonly --manual --no-eff-email --manual-auth-hook /certbot-auth-hook.sh --manual-post-hook /certbot-cleanup-hook.sh --preferred-challenges dns -d *.${DOMAIN_NAME} -d ${DOMAIN_NAME} --agree-tos -m ${MAIL_ADDRESS} --force-renewal
