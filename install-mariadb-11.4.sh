5338cb0eca79ef2a74cb88496797a26f91205810
Kangjun Heo <kheo@sidelcorp.com>
2024-11-06 15:25:42 +0900

    Create install-mariadb-11.4.sh

---
+++ install-mariadb-11.4.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! $UID -eq 0 ]; then
+    echo 'superuser required'
+    exit;
+fi
+
+wget https://raw.githubusercontent.com/0x00000FF/shell-tools/refs/heads/master/yum.repos.d/mariadb-11.4.repo -o /etc/yum.repos.d/MariaDB.repo
+dnf -y update
+
+sudo dnf install -y MariaDB-server MariaDB-client
